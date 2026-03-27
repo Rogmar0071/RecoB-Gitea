a8579bb8cfad0849902cc3a9d0b2d4d7d91580c0
Vicente Duarte <vicente.duarte@tecnico.ulisboa.pt>
2026-03-27 21:37:38 +0000

    Fix #36525: dismiss approvals when re-requesting
    When the same reviewer is requested again, branch
    
    protection can now dismiss previous approvals before
    
    the new request is shown.
    
    Wire the new branch-protection flag through the model,
    
    API, web form, and UI. Add integration coverage for
    
    the branch-protection round-trip and pull-review bug.
    
    Signed-off-by: Vicente Duarte <vicente.duarte@tecnico.ulisboa.pt>

--- services/forms/repo_form.go
+++ services/forms/repo_form.go
@@ -186,6 +186,7 @@ type ProtectBranchForm struct {
 	BlockOnRejectedReviews        bool
 	BlockOnOfficialReviewRequests bool
 	BlockOnOutdatedBranch         bool
+	DismissApprovalsOnRequest     bool
 	DismissStaleApprovals         bool
 	IgnoreStaleApprovals          bool
 	RequireSignedCommits          bool
