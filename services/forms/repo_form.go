16df93d653648815af5ff0afb8b67ee582b55ed6
Vicente Duarte <vicente.duarte@tecnico.ulisboa.pt>
2026-03-27 23:42:31 +0000

    Rename branch protection dismiss approval flag
    Rename the backend field to DismissApprovalsOnReRequest and simplify the pull review regression test into a single top-level test.

--- services/forms/repo_form.go
+++ services/forms/repo_form.go
@@ -186,7 +186,7 @@ type ProtectBranchForm struct {
 	BlockOnRejectedReviews        bool
 	BlockOnOfficialReviewRequests bool
 	BlockOnOutdatedBranch         bool
-	DismissApprovalsOnRequest     bool
+	DismissApprovalsOnReRequest   bool
 	DismissStaleApprovals         bool
 	IgnoreStaleApprovals          bool
 	RequireSignedCommits          bool
