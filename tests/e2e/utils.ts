icyavocado/gitea

Add e2e tests for multiple projects per issue feature
Add Playwright tests to verify the multi-project functionality including:
- Creating a project via UI
- Assigning an issue to multiple projects via sidebar
- Creating an issue with multiple projects pre-selected
- Filtering issues by multiple projects (?projects=ID1,ID2)
- Filtering issues by single project (?project=ID) for backward compatibility
- Removing an issue from one project while keeping others
- Filtering issues with no project (project=-1)
- Closing a project and viewing in closed projects list

Also adds helper functions to tests/e2e/utils.ts:
- createProject(): Creates projects via UI (no API available)
- apiCreateIssue(): Creates issues via API with optional project assignments

tests/e2e/utils.ts
@@ -43,6 +43,57 @@ export async function apiDeleteOrg(requestContext: APIRequestContext, name: stri
   }), 'apiDeleteOrg');
 }
 
+export async function createProject(
+  page: Page,
+  {owner, repo, title}: {owner: string; repo: string; title: string},
+): Promise<{id: number}> {
+  // Navigate to new project page
+  await page.goto(`/${owner}/${repo}/projects/new`);
+
+  // Fill in project details
+  await page.getByLabel('Title').fill(title);
+
+  // Submit the form
+  await page.getByRole('button', {name: 'Create Project'}).click();
+
+  // Wait for redirect to projects list
+  await page.waitForURL(new RegExp(`/${owner}/${repo}/projects$`));
+
+  // Extract the project ID from the project link in the list
+  const projectLink = page.locator('.milestone-list .milestone-card').filter({hasText: title}).locator('a').first();
+  const href = await projectLink.getAttribute('href');
+  const match = /\/projects\/(\d+)/.exec(href || '');
+  const id = match ? parseInt(match[1]) : 0;
+
+  return {id};
+}
+
+export async function apiCreateIssue(
+  requestContext: APIRequestContext,
+  {owner, repo, title, body, projects}: {
+    owner: string;
+    repo: string;
+    title: string;
+    body?: string;
+    projects?: number[];
+  },
+): Promise<{index: number}> {
+  let result: {index: number} = {index: 0};
+  await apiRetry(async () => {
+    const response = await requestContext.post(`${apiBaseUrl()}/api/v1/repos/${owner}/${repo}/issues`, {
+      headers: apiHeaders(),
+      data: {title, body: body || '', projects: projects || []},
+    });
+    if (response.ok()) {
+      const json = await response.json();
+      // API returns "number" field for the issue index
+      result = {index: json.number};
+    }
+    return response;
+  }, 'apiCreateIssue');
+  return result;
+}
+
 export async function clickDropdownItem(page: Page, trigger: Locator, itemText: string) {
   await trigger.click();
   await page.getByText(itemText).click();

Rogmar0071/RecoB-Gitea

tests/e2e/utils.ts
@@ -1,6 +1,6 @@
 import {env} from 'node:process';
 import {expect} from '@playwright/test';
-import type {APIRequestContext, Page} from '@playwright/test';
+import type {APIRequestContext, Locator, Page} from '@playwright/test';
 
 /** Generate a random alphanumeric string. */
 export function randomString(length: number): string {
@@ -47,13 +47,6 @@ export async function apiCreateRepo(requestContext: APIRequestContext, {name, au
   }), 'apiCreateRepo');
 }
 
-export async function apiCreateIssue(requestContext: APIRequestContext, owner: string, repo: string, {title, headers}: {title: string; headers?: Record<string, string>}) {
-  await apiRetry(() => requestContext.post(`${baseUrl()}/api/v1/repos/${owner}/${repo}/issues`, {
-    headers: headers || apiHeaders(),
-    data: {title},
-  }), 'apiCreateIssue');
-}
-
 export async function apiStartStopwatch(requestContext: APIRequestContext, owner: string, repo: string, issueIndex: number, {headers}: {headers?: Record<string, string>} = {}) {
   await apiRetry(() => requestContext.post(`${baseUrl()}/api/v1/repos/${owner}/${repo}/issues/${issueIndex}/stopwatch/start`, {
     headers: headers || apiHeaders(),
@@ -97,6 +90,62 @@ export async function apiDeleteUser(requestContext: APIRequestContext, username:
   await apiRetry(() => requestContext.delete(`${baseUrl()}/api/v1/admin/users/${username}?purge=true`, {
     headers: apiHeaders(),
   }), 'apiDeleteUser');
+}
+
+export async function createProject(
+  page: Page,
+  {owner, repo, title}: {owner: string; repo: string; title: string},
+): Promise<{id: number}> {
+  // Navigate to new project page
+  await page.goto(`/${owner}/${repo}/projects/new`);
+
+  // Fill in project details
+  await page.getByLabel('Title').fill(title);
+
+  // Submit the form
+  await page.getByRole('button', {name: 'Create Project'}).click();
+
+  // Wait for redirect to projects list
+  await page.waitForURL(new RegExp(`/${owner}/${repo}/projects$`));
+
+  // Extract the project ID from the project link in the list
+  const projectLink = page.locator('.milestone-list .milestone-card').filter({hasText: title}).locator('a').first();
+  const href = await projectLink.getAttribute('href');
+  const match = /\/projects\/(\d+)/.exec(href || '');
+  const id = match ? parseInt(match[1]) : 0;
+
+  return {id};
+}
+
+export async function apiCreateIssue(
+  requestContext: APIRequestContext,
+  {owner, repo, title, body, projects}: {
+    owner: string;
+    repo: string;
+    title: string;
+    body?: string;
+    projects?: number[];
+  },
+): Promise<{index: number}> {
+  let result: {index: number} = {index: 0};
+  await apiRetry(async () => {
+    const response = await requestContext.post(`${baseUrl()}/api/v1/repos/${owner}/${repo}/issues`, {
+      headers: apiHeaders(),
+      data: {title, body: body || '', projects: projects || []},
+    });
+    if (response.ok()) {
+      const json = await response.json();
+      // API returns "number" field for the issue index
+      result = {index: json.number};
+    }
+    return response;
+  }, 'apiCreateIssue');
+  return result;
+}
+
+export async function clickDropdownItem(page: Page, trigger: Locator, itemText: string) {
+  await trigger.click();
+  await page.getByText(itemText).click();
 }
 
 export async function loginUser(page: Page, username: string) {