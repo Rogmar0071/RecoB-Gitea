go-gitea/gitea

Improve string join code for projects id

routers/web/repo/issue_new.go
@@ -272,13 +272,12 @@ func ValidateRepoMetasForNewIssue(ctx *context.Context, form forms.CreateIssueFo
 	allProjects := append(slices.Clone(pageMetaData.ProjectsData.OpenProjects), pageMetaData.ProjectsData.ClosedProjects...)
 	candidateProjects := toSet(allProjects, func(project *project_model.Project) int64 { return project.ID })
 	inputProjectIDs, _ := base.StringsToInt64s(strings.Split(form.ProjectIDs, ","))
-	var projectIDStrings []string
-	for _, inputProjectID := range inputProjectIDs {
-		if candidateProjects.Contains(inputProjectID) {
-			projectIDStrings = append(projectIDStrings, strconv.FormatInt(inputProjectID, 10))
+	pageMetaData.ProjectsData.SelectedProjectID = util.JoinSlice(inputProjectIDs, func(v int64) string {
+		if candidateProjects.Contains(v) {
+			return strconv.FormatInt(v, 10)
 		}
-	}
-	pageMetaData.ProjectsData.SelectedProjectID = strings.Join(projectIDStrings, ",")
+		return ""
+	})
 
 	// prepare assignees
 	candidateAssignees := toSet(pageMetaData.AssigneesData.CandidateAssignees, func(user *user_model.User) int64 { return user.ID })

routers/web/repo/issue_page_meta.go
@@ -16,6 +16,7 @@ import (
 	user_model "code.gitea.io/gitea/models/user"
 	"code.gitea.io/gitea/modules/container"
 	"code.gitea.io/gitea/modules/optional"
+	"code.gitea.io/gitea/modules/util"
 	shared_user "code.gitea.io/gitea/routers/web/shared/user"
 	"code.gitea.io/gitea/services/context"
 	issue_service "code.gitea.io/gitea/services/issue"
@@ -161,11 +162,9 @@ func (d *IssuePageMetaData) retrieveAssigneesData(ctx *context.Context) {
 
 func (d *IssuePageMetaData) retrieveProjectsDataForIssueWriter(ctx *context.Context) {
 	if d.Issue != nil && len(d.Issue.Projects) > 0 {
-		ids := make([]string, 0, len(d.Issue.Projects))
-		for _, a := range d.Issue.Projects {
-			ids = append(ids, strconv.FormatInt(a.ID, 10))
-		}
-		d.ProjectsData.SelectedProjectID = strings.Join(ids, ",")
+		d.ProjectsData.SelectedProjectID = util.JoinSlice(d.Issue.Projects, func(v *project_model.Project) string {
+			return strconv.FormatInt(v.ID, 10)
+		})
 	}
 	d.ProjectsData.OpenProjects, d.ProjectsData.ClosedProjects = retrieveProjectsInternal(ctx, ctx.Repo.Repository)
 }

Rogmar0071/RecoB-Gitea

routers/web/repo/issue_new.go
@@ -271,13 +271,12 @@ func ValidateRepoMetasForNewIssue(ctx *context.Context, form forms.CreateIssueFo
 	allProjects := append(slices.Clone(pageMetaData.ProjectsData.OpenProjects), pageMetaData.ProjectsData.ClosedProjects...)
 	candidateProjects := toSet(allProjects, func(project *project_model.Project) int64 { return project.ID })
 	inputProjectIDs, _ := base.StringsToInt64s(strings.Split(form.ProjectIDs, ","))
-	var projectIDStrings []string
-	for _, inputProjectID := range inputProjectIDs {
-		if candidateProjects.Contains(inputProjectID) {
-			projectIDStrings = append(projectIDStrings, strconv.FormatInt(inputProjectID, 10))
+	pageMetaData.ProjectsData.SelectedProjectIDs = util.JoinSlice(inputProjectIDs, func(v int64) string {
+		if candidateProjects.Contains(v) {
+			return strconv.FormatInt(v, 10)
 		}
-	}
-	pageMetaData.ProjectsData.SelectedProjectIDs = strings.Join(projectIDStrings, ",")
+		return ""
+	})
 
 	// prepare assignees
 	candidateAssignees := toSet(pageMetaData.AssigneesData.CandidateAssignees, func(user *user_model.User) int64 { return user.ID })

routers/web/repo/issue_page_meta.go
@@ -16,6 +16,7 @@ import (
 	user_model "code.gitea.io/gitea/models/user"
 	"code.gitea.io/gitea/modules/container"
 	"code.gitea.io/gitea/modules/optional"
+	"code.gitea.io/gitea/modules/util"
 	shared_user "code.gitea.io/gitea/routers/web/shared/user"
 	"code.gitea.io/gitea/services/context"
 	issue_service "code.gitea.io/gitea/services/issue"
@@ -202,11 +203,9 @@ func (d *IssuePageMetaData) retrieveProjectData(ctx *context.Context) {
 
 func (d *IssuePageMetaData) retrieveProjectsDataForIssueWriter(ctx *context.Context) {
 	if d.Issue != nil && len(d.Issue.Projects) > 0 {
-		ids := make([]string, 0, len(d.Issue.Projects))
-		for _, a := range d.Issue.Projects {
-			ids = append(ids, strconv.FormatInt(a.ID, 10))
-		}
-		d.ProjectsData.SelectedProjectIDs = strings.Join(ids, ",")
+		d.ProjectsData.SelectedProjectIDs = util.JoinSlice(d.Issue.Projects, func(v *project_model.Project) string {
+			return strconv.FormatInt(v.ID, 10)
+		})
 	}
 	d.ProjectsData.OpenProjects, d.ProjectsData.ClosedProjects = retrieveProjectsInternal(ctx, ctx.Repo.Repository)
 }